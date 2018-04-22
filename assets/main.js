ccf9973b558a50158c63a177d31957a8a3314e00
jpex <james@jcoyle.com>
2018-04-21 18:18:11 -0700

    JS file cleanup

--- assets/main.js
+++ assets/main.js
@@ -6,32 +6,16 @@ const mqTablet    = 768,
       mqDesktop   = 1280,
       mqLgDesktop = 1700;
 
-const work1  = Math.round($('.work-1').offset().top),
-      work2  = Math.round($('.work-2').offset().top),
-      work3  = Math.round($('.work-3').offset().top);
-
-let work1Pos = work1 + gutter,
-    work2Pos = work2 + gutter,
-    work3Pos = work3 + gutter;
-
 function mq() {
   pageWidth = $(window).width();
   if (pageWidth < mqTablet) { gutter = 40; }
   else if (pageWidth >= mqTablet && pageWidth < mqLgDesktop) { gutter = 70; }
   else { gutter = 120; }
-  work1Pos = work1 + gutter;
-  work2Pos = work2 + gutter;
-  work3Pos = work3 + gutter;
 } mq();
-
-
-//////////
 
 const w1 = $('.work-1'), w1top = w1.position().top;
 const w2 = $('.work-2'), w2top = w2.position().top;
 const w3 = $('.work-3'), w3top = w3.position().top;
-
-console.log(gutter + " " + w1top + " " + w2top + " " + w3top);
 
 $(window).scroll(function(){
   const st = $(window).scrollTop();
@@ -42,34 +26,6 @@ $(window).scroll(function(){
   if (st >= w2top - gutter) { w3.addClass('fix'); }
   else { w3.removeClass('fix'); }
 });
-
-//////////
-// $(window).on("scroll", function() {
-//   const scroll = Math.round($(window).scrollTop());
-//   if (scroll < work1Pos) {
-//     console.log("fire this if statement " + scroll + " " + work2Pos);
-//     $('.work').removeClass('fix');
-//     $('.work-1').addClass('fix');
-//   } else if (scroll < work2Pos) {
-//     console.log("fire this ELSE/if statement");
-//     $('.work').removeClass('fix');
-//     $('.work-2').addClass('fix');
-//   } else {
-//     console.log("fire this ELSE ONLY statement");
-//     $('.work').removeClass('fix');
-//   }
-// });
-
-
-// // var rellax1 = new Rellax('.rellax', {
-// //   speed: 2
-// // });
-// var rellax2 = new Rellax('.rellax2', {
-//   speed: 6
-// });
-// var rellax3 = new Rellax('.rellax3', {
-//   speed: 6
-// });
 
 $('#works > div').on("click", function() {
   let   that   = $(this);
