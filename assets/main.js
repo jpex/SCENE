d785b74948893003b59bfc6f84493d3a35b5b5e1
jpex <james@jcoyle.com>
2018-04-23 17:36:39 -0700

    getting 'page A' and 'page B' to seemlessly transition

--- assets/main.js
+++ assets/main.js
@@ -13,9 +13,12 @@ function mq() {
   else { gutter = 120; }
 } mq();
 
-const w1 = $('.work-1'), w1top = w1.position().top;
-const w2 = $('.work-2'), w2top = w2.position().top;
-const w3 = $('.work-3'), w3top = w3.position().top;
+
+
+  const w1 = $('.work-1'), w1top = w1.position().top;
+  const w2 = $('.work-2'), w2top = w2.position().top;
+  const w3 = $('.work-3'), w3top = w3.position().top;
+
 
 $(window).scroll(function(){
   if (scrollControl) {
@@ -33,19 +36,94 @@ function fixer() {
   else { w3.removeClass('fix'); }
 }
 
-$('#works > div').on("click", function() {
-  let   that   = $(this);
+// $('#works > div').on("click", function() {
+//   let   that   = $(this);
+//   const scroll = that.offset().top + gutter;
+//   const time   = Math.abs($(window).scrollTop() - scroll) + 300;
+//   scrollControl = false;
+//
+//   $(this).removeClass('fix');
+//   $('html, body').animate({
+//     scrollTop: that.offset().top - gutter
+//   }, time).promise().done(function() {
+//     $('#works > .work').addClass('hide');
+//     that.removeClass('hide').addClass('grow');
+//     $('#works').addClass('appear');
+//     $('.hud-top').addClass('transparent');
+//   });
+// });
+
+function test(element) {
+  console.log("test ele");
+  let   that   = $('.work-1');
   const scroll = that.offset().top + gutter;
   const time   = Math.abs($(window).scrollTop() - scroll) + 300;
   scrollControl = false;
 
-  $(this).removeClass('fix');
+  that.removeClass('fix');
   $('html, body').animate({
     scrollTop: that.offset().top - gutter
   }, time).promise().done(function() {
-    $('#works > .work').addClass('hide');
+    $('#works .work').addClass('hide');
     that.removeClass('hide').addClass('grow');
     $('#works').addClass('appear');
     $('.hud-top').addClass('transparent');
   });
+  return true;
+}
+
+let ele = $('.work-1');
+
+
+
+$('document').ready(function(){
+  var transEffect = Barba.BaseTransition.extend({
+      start: function(){
+        const that = this;
+        $.when(test()).done(function() {
+          setTimeout(function(){
+            that.newContainerLoading
+              .then(val => {
+                that.newPage($(that.newContainer))
+              });
+          }, 1500);
+        });
+
+      },
+      newPage: function(nc) {
+
+        var _this = this;
+        nc.addClass('page-fixed');
+        // $('#barba-wrapper').addClass('fixed stable');
+        // $(_this.oldContainer).addClass('fixed');
+        nc.find('.hide').removeClass('hide');
+        nc.fadeIn(20000).promise().done(() => {
+          nc.css('visibility','visible');
+          $(_this.oldContainer).fadeOut(1000, function(){
+            _this.done();
+          })
+        });
+      },
+      test2: function() {
+        console.log("test ele");
+        let   that   = $('.work-1');
+        const scroll = that.offset().top + gutter;
+        const time   = Math.abs($(window).scrollTop() - scroll) + 300;
+        scrollControl = false;
+
+        that.removeClass('fix');
+        $('html, body').animate({
+          scrollTop: that.offset().top - gutter
+        }, time).promise().done(function() {
+          $('#works > .work').addClass('hide');
+          that.removeClass('hide').addClass('grow');
+          $('#works').addClass('appear');
+          $('.hud-top').addClass('transparent');
+        });
+      }
+  });
+  Barba.Pjax.getTransition = function() {
+    return transEffect;
+  }
+  Barba.Pjax.start();
 });
